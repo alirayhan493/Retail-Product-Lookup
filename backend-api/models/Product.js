56bfbc18c77751547c7599901670e457f74af62e
Rayhan Ali <rayhanali@Rayhans-MBP.fios-router.home>
2026-08-06 21:13:24 -0400

    Connected MongoDB and created product model

---
+++ backend-api/models/Product.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    upc: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    brand: {
+      type: String,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
--- backend-api/server.js
+++ backend-api/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch((error) => {
+    console.log(error);
+})
 
 app.get("/", (req, res) => {
     res.send("Retail Product Lookup API Running");
